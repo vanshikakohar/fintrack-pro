2777483ce3a40ffd4a3a53e8c5f061eb24bc9ddd
Vanshika Kohar <vanshikakohar78@gmail.com>
2026-05-21 04:26:31 +0530

    fixed production api

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,4 +1,8 @@
 // export const API_BASE_URL = "http://localhost:5000/api";
 
-const API = "/api";
+const API = "https://fintrack-backend-fnq6.onrender.com/api";
+
 export default API;
+
+// const API = "/api";
+// export default API;
