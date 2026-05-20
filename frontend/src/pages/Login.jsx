97b9757d82507bcfa514334e6fc7681e0b4a30f8
Vanshika Kohar <vanshikakohar78@gmail.com>
2026-05-21 04:49:26 +0530

    fixed login localhost issue

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -11,8 +11,7 @@ const Login = () => {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const res = await fetch("http://localhost:5000/api/auth/login", {
-
+      const res = await fetch(`${API}/auth/login`, {
   method: "POST",
   headers: { "Content-Type": "application/json" },
   body: JSON.stringify({ email, password }),
